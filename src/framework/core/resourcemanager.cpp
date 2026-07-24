8508e8ea4ce476f063ed3b8caae321557eb17b24
matheus  brito <matheusbritto@MacBook-Air-de-matheus.local>
2026-07-24 19:09:31 -0300

    Fix macOS app bundle resource lookup

--- src/framework/core/resourcemanager.cpp
+++ src/framework/core/resourcemanager.cpp
@@ -328,6 +328,7 @@ bool ResourceManager::discoverWorkDir(const std::string& existentFile)
     std::string possiblePaths[] = { g_platform.getCurrentDir(),
                                     g_resources.getBaseDir(),
                                     g_resources.getBaseDir() + "/game_data/",
+                                    g_resources.getBaseDir() + "../Resources/game_data/",
                                     g_resources.getBaseDir() + "../",
                                     g_resources.getBaseDir() + "../share/" + g_app.getCompactName() + "/",
                                     };
